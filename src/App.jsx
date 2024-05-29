2eca06f491481774aea8fdefb1c6889d9ec1c1ec
GargiBhise <gargib.2496@gmail.com>
2024-05-29 16:48:09 -0700

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./app.scss";
+import React from "react";
+
+const App = () => {
+  return <div>
+    <section>About me</section>
+    <section>Skills</section>
+    <section>Experience</section>
+    <section>Education</section>
+    <section>Projects</section>
+    <section>Contact</section>
+  </div>;
+};
+
+export default App;
