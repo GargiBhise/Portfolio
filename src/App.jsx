a1b18f27aa47345f35f418888533bf6744b233b6
GargiBhise <gargib.2496@gmail.com>
2024-05-31 13:30:29 -0700

    Added logo and animation for the header

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./app.scss";
+import Test from "./Test"
 import React from "react";
 import Navbar from "./components/navbar/Navbar";
 
@@ -7,12 +8,14 @@ const App = () => {
     <section>
       <Navbar />
     </section>
+    
     <section>Skills</section>
     <section>Experience</section>
     <section>Education</section>
     <section>Projects</section>
     <section>Contact</section>
-  </div>;
-};
+
+</div>
+}
 
 export default App;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,12 +1,18 @@
 import "./navbar.scss";
 import React from 'react'
+import { motion } from 'framer-motion'
+import "./sidebar/Sidebar.jsx";
 
 const Navbar = () => {
   return (
     <div className="navbar"> 
-      {/* Sidebar */}
+  
+      <Sidebar/>
       <div className="wrapper">
-        <span>Welcome to my page</span>
+        <motion.span initial = {{opacity:0, scale:0.5}}
+                    animate = {{opacity:1, scale:1}}
+                    transition = {{duration:0.5}}
+        >Welcome to my page</motion.span>
         <div className="social">
           <a href=""><img src="/linkedin.png" alt="LinkedIn"></img></a>
           <a href=""><img src="/github1.png" alt="Github"></img></a>
